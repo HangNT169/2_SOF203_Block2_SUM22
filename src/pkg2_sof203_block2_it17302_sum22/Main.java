ea3ae26e01857bc11aa0547ddcdc319c49e44e08
HangNT169 <hN160499aA@>
2022-05-04 23:34:01 +0700

    Init project

---
+++ src/pkg2_sof203_block2_it17302_sum22/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2_sof203_block2_it17302_sum22;
+
+/**
+ *
+ * @author hangnt
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
